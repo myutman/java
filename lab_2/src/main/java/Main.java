5c89d0a326f5f5d3551bed309dd36f1e5d6893ed
myutman <myutman@gmail.com>
2017-09-27 16:01:52 +0300

    done lab2

---
+++ lab_2/src/main/java/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created by myutman on 9/27/17.
+ */
+public class Main {
+    public static void main(String[] args){
+        int[][] a = { {0, 0, 0},
+                      {1, 0, 1},
+                      {2, 1, 3}};
+        Matrix mt = new Matrix(a);
+        mt.sortByFirst();
+        mt.output();
+        mt.outputSpitral();
+    }
+}
