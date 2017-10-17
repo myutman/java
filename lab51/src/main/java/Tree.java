486b92b4e1e84eeb9aa6c473f60fdac1f9c205c6
myutman <myutman@gmail.com>
2017-10-17 10:21:34 +0300

    done lab5.1

--- lab51/src/main/java/Tree.java
+++ lab51/src/main/java/Tree.java
@@ -2,22 +2,39 @@
  * Created by myutman on 10/11/17.
  */
 public class Tree<T extends Comparable<T>> {
-    private class Node{
-        public T value;
-        public int size = 1;
-        public Node left = null, right = null;
 
-        Node(T value){
+    private class Node{
+        private T value;
+        private int size = 1;
+
+        private Node left = null, right = null;
+
+        /**
+         * @param value - value for this Node
+         *
+         * Constructor of Node.
+         */
+        private Node(T value){
             this.value = value;
         }
 
-        void refresh(){
+        /**
+         * Updates number of elements in the Node.
+         */
+        private void refresh(){
             size = (left == null ? 0 : left.size) + (right == null ? 0 : right.size) + 1;
         }
     }
 
     private Node root = null;
 
+    /**
+     * @param node - Node to add element to
+     * @param element - element needed to be added
+     * @return - Node after adding element
+     *
+     * Adds element to the Node and returns new Node with this element added.
+     */
     private Node addToNode(Node node, T element){
         if (node == null){
             return new Node(element);
@@ -31,16 +48,27 @@ public class Tree<T extends Comparable<T>> {
         return node;
     }
 
+    /**
+     * @param element - element needed to be added
+     *
+     * Adds element to the Tree.
+     */
     public void add(T element){
         root = addToNode(root, element);
     }
 
+    /**
+     * @param element - element needed to be checked
+     * @return - true if Tree contains element and false otherwise
+     *
+     * Tells is it true that element is in Tree.
+     */
     public boolean contains(T element){
         Node cur = root;
         while (cur != null){
-            if (cur.value.compareTo(element) < 0){
+            if (cur.value.compareTo(element) > 0){
                 cur = cur.left;
-            } else if (cur.value.compareTo(element) > 0){
+            } else if (cur.value.compareTo(element) < 0){
                 cur = cur.right;
             } else {
                 return true;
@@ -49,6 +77,11 @@ public class Tree<T extends Comparable<T>> {
         return false;
     }
 
+    /**
+     * @return - number of elements in the Tree
+     *
+     * Return number of elements in the Tree.
+     */
     public int size(){
         return (root == null ? 0 : root.size);
     }
