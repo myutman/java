6f78f15dd6b049a32377cb3822636b4a220be352
myutman <myutman@gmail.com>
2017-12-05 23:12:58 +0300

    done lab9

--- lab9/src/main/java/com/github/myutman/java/FirstPartTasks.java
+++ lab9/src/main/java/com/github/myutman/java/FirstPartTasks.java
@@ -2,6 +2,7 @@ package com.github.myutman.java;
 
 import java.util.*;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -27,68 +28,60 @@ public final class FirstPartTasks {
 
     // Список альбомов, в которых есть хотя бы один трек с рейтингом более 95, отсортированный по названию
     public static List<Album> sortedFavorites(Stream<Album> albums) {
-        return albums.filter(x -> x.getTracks().stream().anyMatch(y -> y.getRating() > 95)).sorted((o1, o2) -> o1.getName().compareTo(o2.getName())).collect(Collectors.toList());
+        return albums.filter(x -> x.getTracks().stream().anyMatch(y -> y.getRating() > 95)).
+                sorted((o1, o2) -> o1.getName().compareTo(o2.getName())).collect(Collectors.toList());
     }
 
     // Сгруппировать альбомы по артистам
     public static Map<Artist, List<Album>> groupByArtist(Stream<Album> albums) {
-        Map<Artist, List<Album>> res = new HashMap<>();
-        albums.forEach(x -> {
-            if (res.get(x.getArtist()) == null)
-                res.put(x.getArtist(), new ArrayList<>());
-            res.get(x.getArtist()).add(x);
-        });
-        return res;
+        return albums.collect(Collectors.groupingBy(Album::getArtist, Collectors.toList()));
     }
 
     // Сгруппировать альбомы по артистам (в качестве значения вместо объекта 'Album' использовать его имя)
     public static Map<Artist, List<String>> groupByArtistMapName(Stream<Album> albums) {
-        Map<Artist, List<String>> res = new HashMap<>();
-        albums.forEach(x -> {
-            if (res.get(x.getArtist()) == null)
-                res.put(x.getArtist(), new ArrayList<>());
-            res.get(x.getArtist()).add(x.getName());
-        });
-        return res;
+        return albums.collect(Collectors.groupingBy(Album::getArtist,
+                Collectors.mapping(Album::getName, Collectors.toList())));
     }
 
     // Число повторяющихся альбомов в потоке
     public static long countAlbumDuplicates(Stream<Album> albums) {
-        final Album[] prev = {null};
-        return albums.sorted(((o1, o2) -> o1.getName().compareTo(o2.getName()))).filter(x -> {
-            boolean b = x.equals(prev[0]);
-            prev[0] = x;
-            return b;
-        }).distinct().count();
+        return albums.filter(new Predicate<Album>() {
+            Set<Album> st = new HashSet<>();
+
+            @Override
+            public boolean test(Album album) {
+                return !st.add(album);
+            }
+        }).count();
     }
 
     // Альбом, в котором максимум рейтинга минимален
     // (если в альбоме нет ни одного трека, считать, что максимум рейтинга в нем --- 0)
     public static Optional<Album> minMaxRating(Stream<Album> albums) {
-        Function<List<Track>, Integer> fun = x -> x.isEmpty() ? 0 : x.stream().map(Track::getRating).max((y, z) -> y.compareTo(z)).get();
-        return albums.min((o1, o2) -> fun.apply(o1.getTracks()).compareTo(fun.apply(o2.getTracks())));
+        Function<Album, Integer> fun = x -> x.getTracks().isEmpty() ? 0 : x.getTracks().stream().mapToInt(Track::getRating).max().getAsInt();
+        return albums.min(Comparator.comparing(fun));
     }
 
     // Список альбомов, отсортированный по убыванию среднего рейтинга его треков (0, если треков нет)
     public static List<Album> sortByAverageRating(Stream<Album> albums) {
-        Function<List<Track>, Integer> fun = x -> x.stream().map(Track::getRating).flatMap((x, y) -> (Integer) ((int) x + y));
-        albums.sorted((o1, o2) -> );
+        Function<Album, Double> fun = x -> - x.getTracks().stream().mapToInt(Track::getRating).average().getAsDouble();
+        return albums.sorted(Comparator.comparing(fun)).collect(Collectors.toList());
     }
 
     // Произведение всех чисел потока по модулю 'modulo'
     // (все числа от 0 до 10000)
     public static int moduloProduction(IntStream stream, int modulo) {
-        throw new UnsupportedOperationException();
+        return stream.reduce(1, (x, y) -> (x * y) % modulo);
     }
 
     // Вернуть строку, состояющую из конкатенаций переданного массива, и окруженную строками "<", ">"
     // см. тесты
     public static String joinTo(String... strings) {
-        throw new UnsupportedOperationException();
+        return Arrays.stream(strings).collect(Collectors.joining(", ", "<", ">"));
     }
 
     // Вернуть поток из объектов класса 'clazz'
     public static <R> Stream<R> filterIsInstance(Stream<?> s, Class<R> clazz) {
-        throw new UnsupportedOperationException();
+        return s.filter(clazz::isInstance).map(clazz::cast);
     }
 }
--- lab9/src/main/java/com/github/myutman/java/SecondPartTasks.java
+++ lab9/src/main/java/com/github/myutman/java/SecondPartTasks.java
@@ -1,34 +1,64 @@
 package com.github.myutman.java;
 
-import java.util.List;
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.util.*;
+import java.util.function.DoubleBinaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class SecondPartTasks {
 
     private SecondPartTasks() {}
 
     // Найти строки из переданных файлов, в которых встречается указанная подстрока.
-    public static List<String> findQuotes(List<String> paths, CharSequence sequence) {
-        throw new UnsupportedOperationException();
+    public static List<String> findQuotes(List<String> paths, CharSequence sequence) throws IOException {
+        List<List<String>> ans = new ArrayList<>();
+        for (String path: paths) {
+            ans.add(Files.lines(new File(path).toPath()).flatMap(s -> Arrays.stream(s.split(" "))).filter(s -> s.contains(sequence))
+                    .collect(Collectors.toList()));
+        }
+        return ans.stream().flatMap(List::stream).collect(Collectors.toList());
     }
 
     // В квадрат с длиной стороны 1 вписана мишень.
     // Стрелок атакует мишень и каждый раз попадает в произвольную точку квадрата.
     // Надо промоделировать этот процесс с помощью класса java.util.Random и посчитать, какова вероятность попасть в мишень.
     public static double piDividedBy4() {
-        throw new UnsupportedOperationException();
+        Random random = new Random();
+        return random.doubles(0, 1).limit(20000).reduce(0, new DoubleBinaryOperator() {
+
+            boolean b = false;
+            double last;
+
+            @Override
+            public double applyAsDouble(double left, double right) {
+                double ans;
+                if (b){
+                    ans = left + (last * last + right * right < 1 ? 0.0001 : 0);
+                } else {
+                    last = right;
+                    ans = left;
+                }
+                b = !b;
+                return ans;
+            }
+        });
     }
 
     // Дано отображение из имени автора в список с содержанием его произведений.
     // Надо вычислить, чья общая длина произведений наибольшая.
     public static String findPrinter(Map<String, List<String>> compositions) {
-        throw new UnsupportedOperationException();
+        return compositions.entrySet().stream().max(Comparator
+                .comparingInt(l -> (Integer) l.getValue().stream().mapToInt(String::length).sum())).get().getKey();
     }
 
     // Вы крупный поставщик продуктов. Каждая торговая сеть делает вам заказ в виде Map<Товар, Количество>.
     // Необходимо вычислить, какой товар и в каком количестве надо поставить.
     public static Map<String, Integer> calculateGlobalOrder(List<Map<String, Integer>> orders) {
-        throw new UnsupportedOperationException();
+        return orders.stream().flatMap(m -> m.entrySet().stream()).collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.summingInt(Map.Entry::getValue)));
     }
 }
 
