ee8afd394402ab50d82d93eb9de6bfd6cc2795bf
myutman <myutman@gmail.com>
2017-12-25 00:24:39 +0300

    fixed

--- lab4/src/main/java/com/github/myutman/java/RegExZipper.java
+++ lab4/src/main/java/com/github/myutman/java/RegExZipper.java
@@ -1,57 +1,62 @@
 package com.github.myutman.java;
 
-import jdk.nashorn.internal.runtime.regexp.joni.Regex;
-
 import java.io.*;
 import java.util.Enumeration;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipException;
 import java.util.zip.ZipFile;
-import java.util.zip.ZipOutputStream;
 
 /**
  * Created by myutman on 10/9/17.
+ *
+ * Console application that extracts archives in given directory that matches given regular expression.
  */
 public class RegExZipper {
 
     /**
-     * @param args - arg[0] - name of directory, arg[1] - pattern
-     * @throws IOException
-     *
      * Unzips all the files that match pattern from all the archives in the given directory.
+     * @param args 1) name of directory, 2) pattern
      */
     public static void main(String[] args) throws IOException {
+        if (args.length < 2){
+            throw new IllegalArgumentException();
+        }
+
         String dirName = args[0];
         String pattern = args[1];
         File dir = new File(dirName);
 
-        for (File file : dir.listFiles()){
+        File[] files = dir.listFiles();
+        if (files == null){
+            throw new FileNotFoundException();
+        }
+
+        for (File file : files) {
             if (file.isDirectory()) continue;
-            try (ZipFile zipFile = new ZipFile(file)){
+            try (ZipFile zipFile = new ZipFile(file)) {
                 Enumeration<? extends ZipEntry> entries = zipFile.entries();
                 while (entries.hasMoreElements()){
                     ZipEntry zipEntry = entries.nextElement();
-                    if (zipEntry.getName().matches(pattern)){
-                        if (zipEntry.isDirectory()){
-                            File newFile = new File (dir.getAbsolutePath() + "/" + zipEntry.getName());
+                    if (zipEntry.getName().matches(pattern)) {
+                        if (zipEntry.isDirectory()) {
+                            File newFile = new File(dir.getAbsolutePath() + "/" + zipEntry.getName());
                             newFile.mkdir();
                             continue;
                         }
-                        File newFile = new File (dir.getAbsolutePath() + "/" + zipEntry.getName());
-                        if (!newFile.getParentFile().exists()){
+                        File newFile = new File(dir.getAbsolutePath() + "/" + zipEntry.getName());
+                        if (!newFile.getParentFile().exists()) {
                             newFile.getParentFile().mkdirs();
                         }
-                        FileOutputStream out = new FileOutputStream(newFile);
-                        BufferedInputStream in = new BufferedInputStream(zipFile.getInputStream(zipEntry));
-                        while (in.available() > 0){
-                            byte[] bytes = new byte[in.available()];
-                            in.read(bytes);
-                            out.write(bytes);
+                        try (FileOutputStream out = new FileOutputStream(newFile)) {
+                            BufferedInputStream in = new BufferedInputStream(zipFile.getInputStream(zipEntry));
+                            while (in.available() > 0) {
+                                byte[] bytes = new byte[in.available()];
+                                in.read(bytes);
+                                out.write(bytes);
+                            }
                         }
-                        out.close();
                     }
                 }
-            } catch (ZipException zp){ }
+            }
         }
     }
 }
--- lab4/src/test/java/com/github/myutman/java/RegExZipperTest.java
+++ lab4/src/test/java/com/github/myutman/java/RegExZipperTest.java
@@ -1,15 +1,9 @@
 package com.github.myutman.java;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
-import java.util.Random;
 import java.util.Scanner;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
-import java.util.zip.ZipOutputStream;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by myutman on 10/10/17.
@@ -17,13 +11,11 @@ import static org.junit.Assert.*;
 public class RegExZipperTest {
 
     /**
-     * @throws Exception
-     *
      * Tests RegExZipper functionality on the resource directory and checks correctness.
      */
     @org.junit.Test
     public void testMain() throws Exception {
-        new RegExZipper().main(new String[]{"src/main/resources", ".*tx.*"});
+        RegExZipper.main(new String[]{"src/main/resources", ".*tx.*"});
         Scanner scanner = new Scanner(new File("src/main/resources/input.txt"));
         String s1 = scanner.next();
         assertEquals("abacaba", s1);
