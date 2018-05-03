03c4dfd47b328c070eb5f832ea643fdb563e892c
Mikhail Yutman <myutman@gmail.com>
2018-05-04 01:40:51 +0300

    term2-lab4: fixed bugs, done tests

--- term2/lab4/src/main/java/com/github/myutman/java/FTPClient.java
+++ term2/lab4/src/main/java/com/github/myutman/java/FTPClient.java
@@ -2,39 +2,121 @@ package com.github.myutman.java;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.sun.istack.internal.NotNull;
 
-import javax.swing.plaf.synth.SynthDesktopIconUI;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.lang.reflect.Type;
 import java.net.Socket;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
+/**
+ * Console client that can send queries to server.
+ */
 public class FTPClient {
 
     private static final Type GSON_MAP_TYPE = new TypeToken<Map<String, Object>>(){}.getType();
 
+    /**
+     * Returns the list of files in given directory.
+     * @param socket server to talk to
+     * @param path path to the directory
+     * @return list of files in directory
+     */
+    @NotNull
+    public static List list(Socket socket, String path) {
+        try (DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+             DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream())) {
+            //Output
+            Map<String, Object> outMap = new HashMap<>();
+            int type = 1;
+            outMap.put("type", type);
+            outMap.put("path", path);
+            String data = new Gson().toJson(outMap, GSON_MAP_TYPE);
+            outputStream.writeUTF(data);
+            //Input
+            data = inputStream.readUTF();
+            Map<String, Object> result = new Gson().fromJson(data, GSON_MAP_TYPE);
+            return (List) result.get("list");
+        } catch (IOException e) {
+            return Collections.EMPTY_LIST;
+        }
+    }
+
+    /**
+     * Downloads file from the server and returns its descriptor.
+     * @param socket server to talk to
+     * @param path path to the file
+     * @return new file that is copy of requested
+     */
+    public static File get(Socket socket, String path) {
+        try (DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+             DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream())) {
+            //Output
+            Map<String, Object> outMap = new HashMap<>();
+            int type = 2;
+            outMap.put("type", type);
+            outMap.put("path", path);
+            String data = new Gson().toJson(outMap, GSON_MAP_TYPE);
+            outputStream.writeUTF(data);
+            //Input
+            long size = Long.parseLong(inputStream.readUTF());
+            if (size == 0) {
+                return null;
+            }
+            File outputFile = new File("." + File.separator + path);
+            outputFile.getParentFile().mkdirs();
+            try (FileOutputStream fileOutputStream = new FileOutputStream(outputFile)) {
+                byte[] buffer = new byte[1024];
+                while (size > 0) {
+                    int sz = inputStream.read(buffer);
+                    fileOutputStream.write(buffer, 0, sz);
+                    size -= sz;
+                }
+            }
+            return outputFile;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Console application that helps to send queries.
+     * @param args ip and port (default 127.0.0.1, 6666)
+     */
     public static void main(String[] args) {
+        String ip;
+        int port;
+        if (args.length >= 2) {
+            ip = args[0];
+            port = Integer.parseInt(args[1]);
+        } else {
+            ip = "127.0.0.1";
+            port = 6666;
+        }
         String command;
         String path;
         Scanner scanner = new Scanner(System.in);
         command = scanner.next();
-        while ("list".equals(command) || "get".equals(command)) {
+        while (!"exit".equals(command)) {
             path = scanner.next();
-
-            try (Socket socket = new Socket("192.168.0.48", 6666);
-                 DataInputStream inputStream = new DataInputStream(socket.getInputStream());
-                 DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream())) {
-                Map<String, Object> outMap = new HashMap<>();
-                outMap.put("type", "list".equals(command) ? 1 : 2);
-                outMap.put("path", path);
-                String data = new Gson().toJson(outMap, GSON_MAP_TYPE);
-                System.err.println(data);
-                outputStream.writeUTF(data);
-                System.out.println(inputStream.readUTF());
+            try (Socket socket = new Socket(ip, port)) {
+                if ("list".equals(command)) {
+                    List ans = list(socket, path);
+                    for (Object object: ans) {
+                        Map<String, Object> map = (Map<String, Object>) object;
+                        System.out.printf("(%b, %s) ", map.get("is_dir"), map.get("name"));
+                    }
+                    System.out.println();
+                } else if ("get".equals(command)) {
+                   File file = get(socket, path);
+                   if (file != null) {
+                       System.out.println(file.getAbsolutePath());
+                   } else {
+                       System.out.println("null");
+                   }
+                } else {
+                    System.err.println("Incorrect command.");
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
--- term2/lab4/src/main/java/com/github/myutman/java/FTPServer.java
+++ term2/lab4/src/main/java/com/github/myutman/java/FTPServer.java
@@ -12,59 +12,94 @@ import java.util.*;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
+/**
+ * Server that can conduct two operations, to give a list of all the files in  given directory and to give a file at given path.
+ */
 public class FTPServer {
 
     private static final Type GSON_MAP_TYPE = new TypeToken<Map<String, Object>>(){}.getType();
 
+    /**
+     * Logic of processing queries.
+     * @param socket client to talk to
+     */
+    public static void serverLogic(final Socket socket) throws IOException {
+        try (DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+             DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream())) {
+            String data = inputStream.readUTF();
+            @NotNull Map<String, Object> map = new Gson().fromJson(data, GSON_MAP_TYPE);
+            int type = (int) Math.round((double) map.get("type"));
+            @NotNull String path = (String) map.get("path");
+            System.err.println("type: " + type + ", path: " + path);
+            File file = new File(path);
+            if (type == 1) {
+                Map<String, Object> outMap = new HashMap<>();
+                if (!file.exists() || !file.isDirectory()) {
+                    outMap.put("size", 0);
+                    outMap.put("list", Collections.emptyList());
+                } else {
+                    @NotNull String[] fileList = file.list();
+                    outMap.put("size", fileList.length);
+                    List<Map<String, Object>> list = new ArrayList<>();
+                    for (@NotNull String fileName : fileList) {
+                        File file1 = new File(file.getAbsolutePath() + File.separator + fileName);
+                        Map<String, Object> smallMap = new HashMap<>();
+                        smallMap.put("name", fileName);
+                        smallMap.put("is_dir", file1.isDirectory());
+                        list.add(smallMap);
+                    }
+                    outMap.put("list", list);
+                }
+                data = new Gson().toJson(outMap, GSON_MAP_TYPE);
+                outputStream.writeUTF(data);
+                System.err.println("Written data: " + data);
+                } else if (type == 2) {
+                if (!file.exists() || file.isDirectory()) {
+                    outputStream.writeUTF("0");
+                } else {
+                    long size = file.length();
+                    outputStream.writeUTF(Long.toString(size));
+                    byte[] buffer = new byte[1024];
+                    try (FileInputStream inputStream1 = new FileInputStream(file)) {
+                        while (size > 0) {
+                            int sz = inputStream1.read(buffer);
+                            outputStream.write(buffer, 0, sz);
+                            size -= sz;
+                        }
+                    }
+                }
+            } else {
+                System.err.println("lol");
+                System.exit(0);
+            }
+        } finally {
+            socket.close();
+        }
+    }
+
+    /**
+     * Runs the server.
+     * @param args port (default 6666)
+     */
     public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(6666)) {
+        int port;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+            System.err.println(port);
+        } else {
+            port = 6666;
+        }
+        try (final ServerSocket serverSocket = new ServerSocket(port)) {
             Executor executor = Executors.newSingleThreadExecutor();
             while (true) {
-                try (final Socket socket = serverSocket.accept()) {
-                    executor.execute(() -> {
-                        try (DataInputStream inputStream = new DataInputStream(socket.getInputStream());
-                             DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream())){
-                            @NotNull Map<String, Object> map = new Gson().fromJson(inputStream.readUTF(), GSON_MAP_TYPE);
-                            @NotNull Object type = map.get("type");
-                            @NotNull String path = (String) map.get("path");
-                            File file = new File(path);
-                            Map<String, Object> outMap = new HashMap<>();
-                            if (type.equals(1)) {
-                                if (!file.exists() || !file.isDirectory()) {
-                                    outMap.put("size", 0);
-                                    outMap.put("list", Collections.emptyList());
-                                } else {
-                                    @NotNull String[] fileList = file.list();
-                                    outMap.put("size", fileList.length);
-                                    List<Map<String, Object>> list = new ArrayList<>();
-                                    for (@NotNull String fileName : fileList) {
-                                        File file1 = new File(file.getAbsolutePath() + File.pathSeparator + fileName);
-                                        Map<String, Object> smallMap = new HashMap<>();
-                                        smallMap.put("name", fileName);
-                                        smallMap.put("is_dir", file1.isDirectory());
-                                        list.add(smallMap);
-                                    }
-                                    outMap.put("list", list);
-                                }
-                            } else if (type.equals(2)) {
-                                if (!file.exists() || file.isDirectory()) {
-                                    outMap.put("size", 0);
-                                    outMap.put("content", new byte[]{});
-                                } else {
-                                    outMap.put("size", file.length());
-                                    byte[] buffer = new byte[(int) file.length()];
-                                    try (FileInputStream inputStream1 = new FileInputStream(file)) {
-                                        inputStream1.read(buffer);
-                                        outMap.put("content", buffer);
-                                    }
-                                }
-                            }
-                            outputStream.writeUTF(new Gson().toJson(outMap, GSON_MAP_TYPE));
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    });
-                }
+                final Socket socket = serverSocket.accept();
+                executor.execute(() -> {
+                    try {
+                        serverLogic(socket);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
             }
         } catch (IOException e) {
             e.printStackTrace();
